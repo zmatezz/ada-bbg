35b7bee964494510d1886be1ffc3e4041c8fc36c
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-11-04 01:16:01 -0300

    feat: added login screen and notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
     </Routes>
   );
 }
